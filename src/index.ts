0d6fee9f8aee8ca0c0556830d4f694eb4a03efbf
motephyr <motephyr@gmail.com>
2019-07-25 17:19:15 +0800

    add some comment

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,41 @@
 import DataLoader from 'dataloader'
+
 /**
  * Creates a dataloader for a supplied model.
- * 
- * @param {String} model e.g. 'App/Models/User'
- * @param {String} ref_id e.g. 'order_id'
- * @returns {DataLoaderHelper}
  */
-
 export class DataLoaderHelper {
+
+  /**
+   * get array of objects for cache.
+   */
   queryData: Function
-  constructor (queryData: Function) {
+  constructor(queryData: Function) {
     this.queryData = queryData
   }
 
-  create(model: string) {
+  /**
+   * create model's dataloader.
+   */
+  create(tablename: string): DataLoader<any, object> {
     let batchLoadFn = async (keys: any[]) => {
-      let keys_values = keys.map((x) => x[1])
-      let data = await this.queryData(model, keys[0][0], keys_values)
+      let keys_values = keys.map((x) => x.value)
+      let data = await this.queryData(tablename, keys[0].key, keys_values)
 
-      return this.sortData(data.rows, keys[0][0], keys_values, keys[0][2])
+      return this.getData(data, keys[0].key, keys_values, keys[0].many)
     }
 
     return new DataLoader(batchLoadFn)
   }
 
-
-  sortData(data: any[], ref_id: string, keys: any[], has: any) {
-    console.log(data)
-    if (has === 'many') {
-      return keys.map(id => data.filter(r => r[ref_id] === id))
+  /**
+   * @private
+   * collect all data.
+   */
+  getData(data: any[], ref_id: string, key_ids: number[], many: boolean): object[] {
+    if (!many) {
+      return key_ids.map(id => data.find(r => r[ref_id] === id))
     } else {
-      return keys.map(id => data.find(r => r[ref_id] === id))
+      return key_ids.map(id => data.filter(r => r[ref_id] === id))
     }
   }
 }
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,19 +1,21 @@
 import { DataLoaderHelper } from '../src/index';
 
-
-
 test('init', async () => {
   const Loader = new DataLoaderHelper(async function (model: string, ref_id: string, keys: any[]) {
-    console.log(`*** loading all '${model}' with ids [${keys}] from database`)
+    // console.log(`*** loading all '${model}' with ids [${keys}] from database`)
 
-    return {
-      rows: [{ company_id: 123, company_data: ["company_data_123"] },
-      { company_id: 124, company_data: ["company_data_124"] }]
-    }
+    return [{ id: 1, company_id: 123, company_data: ["company_data_123"] },
+    { id: 2, company_id: 124, company_data: ["company_data_124"] }]
   })
   let users = Loader.create('users')
-  let result = await users.load(['company_id', 123, 'many'])
-  expect(result).toEqual([ { company_id: 123, company_data: [ 'company_data_123' ] } ]);
+  let hasMany = await users.load({ key: 'company_id', value: 123, many: true })
+  expect(hasMany).toEqual([{ id: 1, company_id: 123, company_data: ['company_data_123'] }]);
+
+  let hasOne = await users.load({ key: 'company_id', value: 123 })
+  expect(hasOne).toEqual({ id: 1, company_id: 123, company_data: ['company_data_123'] });
+  
+  let belongsTo = await users.load({ key: 'id', value: 2 })
+  expect(belongsTo).toEqual({ id: 2, company_id: 124, company_data: ['company_data_124'] });
 
 });
 
